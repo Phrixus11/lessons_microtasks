8f9e85a631eeab690402df8aaa010f831c48441c
YuryR <myaccount136@gmail.com>
2025-05-15 20:01:01 +0300

    useEffect cleanup

--- src/kabzda/useEffect.stories.tsx
+++ src/kabzda/useEffect.stories.tsx
@@ -59,33 +59,83 @@ export const UseEffectSetTimeout = () => {
         {counter}
         <button onClick={() => setFake(prevState => prevState + 1)}>fake+</button>
         {fake}
-
       </>
   )
 }
 
+export const UseEffectSetInterval = () => {
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+
+    const intervalId = setInterval(() => {
+
+      console.log('tick')
+    }, 1000)
+    return () => {
+      console.log('clear interval')
+      clearInterval(intervalId)
+    }
+  }, [counter]);
+
+  return (
+      <>
+        <hr/>
+        <button onClick={() => setCounter(counter + 1)}>count++</button>
+      </>
+  )
+}
+
+
 export const UseEffectSetInterval_Clock = () => {
 
-  const [counter, setCounter] = useState(()=>{
+  const [counter, setCounter] = useState(() => {
     return new Date().toLocaleTimeString();
   });
+
   const [startTimer, setStartTimer] = useState(false);
+
+  //создал эту переменную, потому что хотел чистить инвервал по кнопке в разметке в use Ref
   const timerId = useRef<NodeJS.Timeout | null>(null)
 
   useEffect(() => {
 
     timerId.current = setInterval(() => {
-      setCounter(() =>  new Date().toLocaleTimeString());
+      setCounter(() => new Date().toLocaleTimeString());
     }, 1000)
-    return () => clearInterval(timerId.current? timerId.current: '')
+    return () => clearInterval(timerId.current ? timerId.current : '')
   }, [startTimer]);
 
   return (
       <>
         Clock: {counter}
         <hr/>
-        <button onClick={()=>setStartTimer(!startTimer)}>start clock</button>
-        <button onClick={()=>clearInterval(timerId.current? timerId.current: '')}>stop clock</button>
+        <button onClick={() => setStartTimer(!startTimer)}>start clock</button>
+        <button onClick={() => clearInterval(timerId.current ? timerId.current : '')}>stop clock</button>
       </>
+  )
+}
+
+
+export const KeyTrackerExample = () => {
+  const [text, setText] = useState('');
+
+  //зачистка слушателя в UseEffect после выхода из компоненты
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      setText(prevState => prevState + e.key);
+      console.log(e.key)
+    }
+    window.addEventListener('keypress', handler )
+    return () => {
+      window.removeEventListener('keypress', handler )
+    }
+  }, [])
+
+  return (
+      <div>
+        Text: {text}
+      </div>
   )
 }
